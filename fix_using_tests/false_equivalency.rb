a87c25dcf17739a0d0dabe3f59b64c6c50d1ddd9
interpreted-validator-6256 <danielle.leppertsimenauer@gmail.com>
2020-11-19 03:00:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,7 +12,7 @@ end
 
 def selection(num)
   if num == 1 
-     return "YUM YUM MUNCH MUNCH MUNCH"
+     print "YUM YUM MUNCH MUNCH MUNCH"
   elsif num == 2 
      return "HAM HAM HAM IN MY TUMMY"
   end
