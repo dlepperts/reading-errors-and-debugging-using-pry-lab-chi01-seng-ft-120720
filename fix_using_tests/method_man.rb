d27acd712b39f7ce2153bb6945b284292bf7f849
interpreted-validator-6256 <danielle.leppertsimenauer@gmail.com>
2020-11-19 02:24:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,7 +1,6 @@
 require "pry"
 
 def start_game(player1, player2)
-  #puts "Hello #{player1} & #{player2}"
   return "Hello #{player1} & #{player2}"
 end
 
@@ -9,4 +8,4 @@ def play_game(player1, player2)
   return "#{player1} is better than #{player2}"
 end
 
-start_game('Danielle','Jonah')
+\
